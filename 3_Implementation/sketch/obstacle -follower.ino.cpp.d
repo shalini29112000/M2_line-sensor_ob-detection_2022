ad1139234218ab6087cdac920f3951fbab63860c
Shalini <85799635+shalini29112000@users.noreply.github.com>
2022-04-21 19:40:46 +0530

    Create obstacle -follower.ino.cpp.d

---
+++ 3_Implementation/sketch/obstacle -follower.ino.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\PERSONAL\AppData\Local\Temp\arduino_build_410181\sketch\obstacle-follower.ino.cpp.o: \
+ C:\Users\PERSONAL\AppData\Local\Temp\arduino_build_410181\sketch\obstacle-follower.ino.cpp \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\PERSONAL\Downloads\arduino-1.8.19-windows\arduino-1.8.19\hardware\arduino\avr\variants\standard/pins_arduino.h
